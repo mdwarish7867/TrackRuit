84dab2a6eee146c2963011cf2400fb7614082f3e
MD WARISH ANSARI <warishansari018@proton.me>
2025-07-11 00:29:53 +0530

    Fix: ensure clean deploy structure

--- backend/server.js
+++ backend/server.js
@@ -38,7 +38,7 @@ if (process.env.NODE_ENV === "development") {
 }
 
 // Routes
-const mainRouter = require("./routes/index.js");
+const mainRouter = require("./routes");
 
 
 app.use("/api", mainRouter);
